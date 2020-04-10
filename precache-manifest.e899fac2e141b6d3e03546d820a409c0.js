c27606adc8508ab6235fdc1236c2e5827fdf8800
Ninad Daithankar <ninad.nd15@gmail.com>
2020-04-10 14:47:27 +0530

    Updates

---
+++ precache-manifest.e899fac2e141b6d3e03546d820a409c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048f920999a604f4e3cc9259f5b1afa0",
+    "url": "/ninaddaithankar.github.io/my-portfolio-react/index.html"
+  },
+  {
+    "revision": "98069831870f16ecce20",
+    "url": "/ninaddaithankar.github.io/my-portfolio-react/static/css/main.214b462a.chunk.css"
+  },
+  {
+    "revision": "a24885bf9b0d72388701",
+    "url": "/ninaddaithankar.github.io/my-portfolio-react/static/js/2.587a403a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ninaddaithankar.github.io/my-portfolio-react/static/js/2.587a403a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98069831870f16ecce20",
+    "url": "/ninaddaithankar.github.io/my-portfolio-react/static/js/main.0f2ac7f9.chunk.js"
+  },
+  {
+    "revision": "77ef7d22cce8fd771fa7",
+    "url": "/ninaddaithankar.github.io/my-portfolio-react/static/js/runtime-main.7a808ba6.js"
+  },
+  {
+    "revision": "32044e92c4960a0e432709634791a1bf",
+    "url": "/ninaddaithankar.github.io/my-portfolio-react/static/media/profile-hi-res.32044e92.JPG"
+  }
+]);
