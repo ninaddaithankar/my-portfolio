95bd29fc4aa9c5e3912d8ba66cc7e72e272e9ed6
Ninad Daithankar <ninad.nd15@gmail.com>
2020-07-03 21:46:09 +0530

    Updates

---
+++ precache-manifest.3d51385b79179a05ceef99f6f4f77dd5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86080ce14216e5216734345987b62f89",
+    "url": "/my-portfolio-react/index.html"
+  },
+  {
+    "revision": "07db9e7c2aae64984c50",
+    "url": "/my-portfolio-react/static/css/main.ecb2ac0a.chunk.css"
+  },
+  {
+    "revision": "71ce3b5dcd005c507bf7",
+    "url": "/my-portfolio-react/static/js/2.b0c8250f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio-react/static/js/2.b0c8250f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07db9e7c2aae64984c50",
+    "url": "/my-portfolio-react/static/js/main.d850b0de.chunk.js"
+  },
+  {
+    "revision": "5bdea9108fd45ad6cc5d",
+    "url": "/my-portfolio-react/static/js/runtime-main.3a38d5f0.js"
+  },
+  {
+    "revision": "cc348b3d4a85e85344414ac4ed09c0e4",
+    "url": "/my-portfolio-react/static/media/portfolio-new.cc348b3d.png"
+  },
+  {
+    "revision": "32044e92c4960a0e432709634791a1bf",
+    "url": "/my-portfolio-react/static/media/profile-hi-res.32044e92.JPG"
+  },
+  {
+    "revision": "a68cf70067558cc9bde98492ff3f5794",
+    "url": "/my-portfolio-react/static/media/smiley.a68cf700.svg"
+  },
+  {
+    "revision": "8ee7fcb932540041e18ee4e034a14a89",
+    "url": "/my-portfolio-react/static/media/spinner.8ee7fcb9.gif"
+  },
+  {
+    "revision": "256afced2e5a58be609627cdc88124f7",
+    "url": "/my-portfolio-react/static/media/svg-portrait-new.256afced.svg"
+  },
+  {
+    "revision": "559b8279b986cd9814b7aa809bf8e8ff",
+    "url": "/my-portfolio-react/static/media/time-1.559b8279.png"
+  },
+  {
+    "revision": "6e80337080e811ee3fa7278660e832e2",
+    "url": "/my-portfolio-react/static/media/time-2.6e803370.png"
+  },
+  {
+    "revision": "dfca9dd213284db906130132d4324ae7",
+    "url": "/my-portfolio-react/static/media/time-32.dfca9dd2.png"
+  },
+  {
+    "revision": "74a035cae3099b3424828355d664d069",
+    "url": "/my-portfolio-react/static/media/unwrapp.74a035ca.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a3d0bb9495ab90d2ff3729e90399221.js"
+  "/my-portfolio-react/precache-manifest.3d51385b79179a05ceef99f6f4f77dd5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
